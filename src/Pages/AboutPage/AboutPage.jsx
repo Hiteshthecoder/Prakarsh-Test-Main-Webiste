779844eb8787a17c4137590a8cd7c5865f0a3326
hitesh <hiteshparmar16320@gmail.com>
2022-12-27 14:42:53 +0530

    Initial Commit

---
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './AboutPage.css'
+
+function AboutPage() {
+    return (
+        <div className='ABOUT-US-MAIN-CONTAINER'>
+                    <div className='ABOUT-US-HEADING' >
+                          <h1 >ABOUT US</h1>
+                      </div>
+          
+                    <div className='aboutrightcontainer' >
+                          <div className='aboutrightcontent'>
+                               <p>Sardar Vallabhbhai Patel Institute Of Technology, Vasad,Has Emerged As The Chieftain Among The Premiere Institutes Of Gujrat For The Last 25 Years.Prakarsh Is One Of The Most Recognized Symposiums All Over Gujrat.</p>
+                              <p>
+                                It Is A Perfect Launch Pad For Tech Geeks To Showcase Their Inteligence And Innovativeness,And A Massive Platform For Various Activities And Events Combined With Lots Of Entertainment.Prakarsh 2023 Will Be Held On 23-25 Feb And Will Feature An Impressive Lineup of 40 Events, Which Will Incorporate Fun Activities With Ardour.
+                                </p>
+                              </div>
+                     </div>
+            
+        </div>
+    )
+}
+
+export default AboutPage
+
+// data-aos="fade-left"   data-aos-delay="300"  data-aos-mirror="true"
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './HomePage.css'
+function HomePage() {
+  return (
+    <div className='HOMEPAGE-MAIN-CONTAINER'>
+      <div className='bgimage'>
+          <img src="assets\hero2.png" alt="bg1"/>
+          </div>
+           <div className='headerline'>
+             <h1> PRAKARSH'<span>23</span></h1>  
+             <h2> ENGAGING mINds, eMpowERiNg sUccEss
+</h2>
+             <p> Prakarsh 23's vision is to assist the rural areas, and villages and to encourage them in the advancement of their lives through technologies developed in Prakarsh by students. </p>
+
+            <button type='button' className='expbtn'><a href='#'>Explore Events  </a> </button>
+
+             {/* <div className='launch'>
+             <h1> 1-2 MARCH</h1>
+           </div> */}
+          </div>
+        
+       
+    </div>  
+  )
+}
+
+export default HomePage
